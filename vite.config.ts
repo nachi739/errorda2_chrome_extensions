b0af068eedc9ec6998655d5b7410d5d304566afe
namiki <namiki081025@gmail.com>
2024-10-29 18:59:16 +0900

    [update]dotenvの記述削除

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./src/manifest.json";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 export default defineConfig({
   base: "./",
